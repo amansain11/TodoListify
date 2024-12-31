264d77e04902dc200280b90bd8d7dd4db28ac98a
amansain11 <sainaman1819@gmail.com>
2024-12-31 16:14:22 +0530

    Add login controller endpoint with it's route, Fixed: add await at missing places in userSchema methods and fixed typos in generate access and refresh token methods

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,21 @@ import apiError from "../utils/apiError.js";
 import apiResponse from "../utils/apiResponse.js";
 import {User} from "../models/users.model.js";
 
+const generateAccessAndRefreshToken = async (userId) =>{
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+    
+        user.refreshToken = refreshToken;
+        user.save({validateBeforeSave: false});
+    
+        return { accessToken, refreshToken };       
+    } catch (error) {
+        throw new apiError(500, "Error while generating refresh and access token", error)
+    }
+}
+
 const registerUser = asyncHandler(async (req, res)=>{
     const {email, username, password} = req.body;
 
@@ -42,6 +57,59 @@ const registerUser = asyncHandler(async (req, res)=>{
     )
 })
 
+const loginUser = asyncHandler(async(req, res)=>{
+    const {email, password} = req.body;
+    
+    if(!(email || password) || (email.trim() || password.trim()) === ""){
+        throw new apiError(400, "email or password is required")
+    }
+
+    const user = await User.findOne({email})
+    
+    if(!user){
+        throw new apiError(404, "User does not exists")
+    }
+
+    // console.log(password)
+    // console.log(user.password)
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+    // console.log(isPasswordValid)
+
+    if(!isPasswordValid){
+        throw new apiError(404, "Invalid user credentials")
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id)
+
+    const loggedInUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    const option = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, option)
+    .cookie("refreshToken", refreshToken, option)
+    .json(
+        new apiResponse(
+            200,
+            {
+                user: loggedInUser,
+                accessToken,
+                refreshToken,
+            },
+            "User logged in Successfully"
+        )
+    )
+
+})
+
 export {
-    registerUser
+    registerUser,
+    loginUser
 }
--- src/models/users.model.js
+++ src/models/users.model.js
@@ -28,7 +28,7 @@ const userSchema = new mongoose.Schema(
 userSchema.pre("save",async function(next){
     if(!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
@@ -37,29 +37,29 @@ userSchema.methods.isPasswordCorrect = async function(password){
 }
 
 userSchema.methods.generateAccessToken = function(){
-    jwt.sign(
+    return jwt.sign(
         {
             _id: this._id,
             email: this.email,
             username: this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
         }
-    ),
-    process.env.ACCESS_TOKEN_SECRET,
-    {
-        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-    }
+    )
 }
 
 userSchema.methods.generateRefreshToken = function(){
-    jwt.sign(
+    return jwt.sign(
         {
             _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
         }
-    ),
-    process.env.REFRESH_TOKEN_SECRET,
-    {
-        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
-    }
+    )
 }
 
 export const User = mongoose.model("User", userSchema)
