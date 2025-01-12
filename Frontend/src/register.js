6214f8e857ef881e365798ac8c9a5bba8fcdcfbd
amansain11 <sainaman1819@gmail.com>
2025-01-12 23:52:34 +0530

    Add todo-page header, merged css file of login-page and register-&-login-page to register-login-page and also moved css section class to it

--- Frontend/src/register.js
+++ Frontend/src/register.js
@@ -1,3 +1,4 @@
+import '../styles/modern-normalize.css';
 import '../styles/style.css';
-import '../styles/register-&-login-page.css';
+import '../styles/register-login-page.css';
 
--- Frontend/src/todo.js
+++ Frontend/src/todo.js
@@ -1,3 +1,3 @@
 import '../styles/modern-normalize.css';
 import '../styles/style.css';
-import '../styles/register-login-page.css';
+import '../styles/todo-subtodo-components/header.css';
