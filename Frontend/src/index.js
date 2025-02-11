8e7ac4a55269efda651ab0474625dca863a50407
amansain11 <sainaman1819@gmail.com>
2025-02-11 16:06:01 +0530

    Add user authentication to home page

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,3 +1,12 @@
 import '../styles/modern-normalize.css';
 import '../styles/style.css';
 import '../styles/home-page.css';
+
+import authUser from "./utils/authenticate-user.js";
+
+(async() => {
+    const session = await authUser()
+    if(session.success){
+        location.href = '/todo-page.html';
+    }
+})()
