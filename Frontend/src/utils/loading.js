57da0724af79b7a163f18b4a72d940a6e192923c
amansain11 <sainaman1819@gmail.com>
2025-02-21 00:01:59 +0530

    remove 'dist' from gitignore

--- Frontend/src/utils/loading.js
+++
@@ -1,9 +0,0 @@
-const loading = ()=>{
-    let loadingBox = document.createElement('div')
-    loadingBox.setAttribute('class', 'loading-box')
-    loadingBox.innerHTML = `<img src="/loading-icon.gif" alt='loading..'>`;
-    
-    return loadingBox;
-}
-
-export default loading;
